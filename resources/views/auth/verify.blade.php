7de1b2dc3db5696f262987ab9d9c96eb30eabf0a
vagrant <vagrant@homestead>
2018-11-12 21:26:56 +0000

    Got things up and running

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -7,6 +7,7 @@
 @stop
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
-@extends('layouts.home')
+@extends('layouts.main')
 
+@section('head')
+
+@include('layouts.partials.head')
+    
+@stop
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    
+    <h2>Dashboard</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <div>
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            <h3>Welcome {{ Auth::user()->name }}</h4>
+
+            You are logged in!
         </div>
-    </div>
 </div>
 @endsection
